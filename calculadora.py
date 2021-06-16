7b61b1f67a0cb0ff7d18714f9d643cf0547c07c3
Martin Fonseca <75582918+Merlinys@users.noreply.github.com>
2021-06-16 18:48:04 -0400

    Calculadora antes de github

---
+++ calculadora.py
@@ -0,0 +1,52 @@
+def resta():
+    resta= input("""
+Con x se termina de sumar.
+Ingrese el numero de la suma: """)
+    if resta == "x":
+        print("Suma terminada")
+    resta = int(resta)
+    print(resta)
+    while True:
+        num = input("""
+Con x se termina de sumar.
+Ingrese el numero de la suma: """)
+        if num == "x":
+            print("Suma terminada")
+            break
+        num = int(num)
+        resta -= num
+        print(resta)
+
+def sumar(suma):
+    suma =[]
+    while True:
+                num = input("""
+Con x se termina de sumar.
+Ingrese el numero de la suma: """)
+                if num == "x":
+                    print("Suma terminada")
+                    break
+                num = int(num)
+                suma.append(num)
+                print(sum(suma))
+
+def run():
+    numero = 0 
+    while numero != 5:
+        numero = int(input("""
+Calculadora:
+1.Sumar
+2.Resta
+3.Multiplicacion
+4.Dividir
+5.Terminar calculadora
+Elija su operacion: """))
+        if numero == 1:
+            print(sumar())
+        if numero == 2:
+            print(resta())
+            
+
+
+if __name__ == "__main__":
+    run()
